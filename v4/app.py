9eb08234e126d38e99372c5d8b5531ba9aee9a7a
dmitrysklyarov <dima@sklyarov.com>
2023-05-30 19:37:19 +0300

    added more info about current status

--- v4/app.py
+++ v4/app.py
@@ -9,6 +9,7 @@ import psycopg2
 import config
 from flask import Flask
 import json
+from trade import Trade
 
 app = Flask(__name__)
 
@@ -33,7 +34,22 @@ def profit():
     profit1 = round(float(profit1), 4)
 
     curs.execute("SELECT sum(quantity) FROM buy WHERE not satisfied")
-    quantity = round(float(curs.fetchone()[0]), 4)
+    inorders = float(curs.fetchone()[0])
+
+    curs.execute("SELECT price FROM buy WHERE not satisfied ORDER BY price DESC LIMIT 1")
+    topprice = round(float(curs.fetchone()[0]))
+
+    bottomprice = 0
+    currentprice = 0
+    quote = 0
+    base = 0
+
+    with Trade() as trade:
+        currentprice = round(trade.getMarketPrice())
+        quote = round(trade.getQuantity(config.getQuote()))
+        base = round(trade.getQuantity(config.getBase()) * currentprice)
+        bottomprice = round(currentprice - config.getIndent() *  quote / (config.getQuantity() * currentprice))
+        inorders = round(currentprice * inorders)
 
     conn.commit()
     curs.close()
@@ -43,5 +59,10 @@ def profit():
                     "profit1" : profit1, 
                     "profit7" : profit7, 
                     "profit30" : profit30,
-                    "quantity" : quantity})
+                    "inorders" : inorders,
+                    "quote" : quote,
+                    "base" : base,
+                    "topprice" : topprice,
+                    'currentprice' : currentprice,
+                    'bottomprice' : bottomprice})
     return j
